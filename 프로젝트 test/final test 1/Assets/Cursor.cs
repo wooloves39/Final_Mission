03ef85a8ce8a22c02143c08764f76397421f219a
Kang Hyun Woong <cdcd8894@naver.com>
2017-09-25 20:37:59 +0900

    유니티 프로젝트 2개
    1번 도형 드로우( 삭제, 판정 미완)
    2번 캐릭터 움직임(3인칭 무빙이라 1인칭으로 보면 어색)

---
+++ 프로젝트 test/final test 1/Assets/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	Vector2 movePosition;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		movePosition = Input.mousePosition;
+		transform.position = movePosition;
+	}
+}
