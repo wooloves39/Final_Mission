17fccd74842663039d1f6d4c36d9b61d6efd5424
Kang Hyun Woong <cdcd8894@naver.com>
2017-10-12 20:44:08 +0900

    2017 10 12 작업내용
    기본공격 구현중 ~ing
    기본 공격을 구현해야 마법의 충돌판정이 가능

---
+++ 프로젝트 test/Test 2/Assets/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+	bool attack = false;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire2")) 
+		{
+			attack = true;
+		}
+			
+	}
+}
