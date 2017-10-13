214c40465d6b272c77683c57d2db158783f5a210
Kang Hyun Woong <cdcd8894@naver.com>
2017-10-13 16:03:05 +0900

    2017 10 13 작업내용
    기본 공격 (투사체 구현)
    아직 충돌체크 안함

--- 프로젝트 test/Test 2/Assets/Attack.cs
+++ 프로젝트 test/Test 2/Assets/Attack.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour {
-	bool attack = false;
+public class Attack :  MonoBehaviour  {
+	public bool attack = false;
+	public float Speed = 5.0f;
+	Vector3 prevposition;
+	FollowCamera followCamera;
 	// Update is called once per frame
+	void Start () {
+		followCamera = FindObjectOfType<FollowCamera>();
+	}
 	void Update () {
-		if (Input.GetButtonDown ("Fire2")) 
+		if (Input.GetButtonDown ("Fire2")&& !attack) 
 		{
 			attack = true;
+			transform.rotation =followCamera.transform.rotation;
+			transform.position =  followCamera.transform.position;
+			prevposition = new Vector3(transform.position.x,transform.position.y,transform.position.z);
 		}
-			
+		if (attack) {
+			transform.Translate (Vector3.forward * Speed * Time.deltaTime * 2, Space.Self);
+			if (Vector3.Distance (prevposition, transform.position) > Speed * 4) {
+				attack = false;
+				transform.position = prevposition + new Vector3(0,-100,0);
+			}
+		}
 	}
 }
