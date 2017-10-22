1061ce10d62ec35d317ecd09f6da7c087ba73bff
Kang Hyun Woong <cdcd8894@naver.com>
2017-10-22 16:10:09 +0900

    2017 10 22 작업내용
    1.  테라리안과 충돌체크 완료
    2.  공격 적과 충돌 시 몬스터 체력 깍이는거 완료
    3.  주석 달아둠
    
    타겟팅 구현중~ing + UI 구현이 먼저 되어야 할것 같음(자세한 내용은 월요일 회의때 함)

--- 프로젝트 test/Test 2/Assets/Attack.cs
+++ 프로젝트 test/Test 2/Assets/Attack.cs
@@ -12,17 +12,22 @@ public class Attack :  MonoBehaviour  {
 		followCamera = FindObjectOfType<FollowCamera>();
 	}
 	void Update () {
+		//마우스 오른쪽 버튼을 누르면 시작 + bool 함수 attack
 		if (Input.GetButtonDown ("Fire2")&& !attack) 
 		{
 			attack = true;
+			//카메라 방향과 위치를 불러온다.
 			transform.rotation =followCamera.transform.rotation;
 			transform.position =  followCamera.transform.position;
 			prevposition = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+
 		}
 		if (attack) {
+			//정면으로 발사
 			transform.Translate (Vector3.forward * Speed * Time.deltaTime * 2, Space.Self);
 			if (Vector3.Distance (prevposition, transform.position) > Speed * 4) {
 				attack = false;
+				//끝나고 캐릭터 y -100으로 이동(동적으로 생성X)
 				transform.position = prevposition + new Vector3(0,-100,0);
 			}
 		}
@@ -33,6 +38,7 @@ public class Attack :  MonoBehaviour  {
 	}
 	void Hit()
 	{
+		//중간에 충돌 발생시 사라지게 만듬.
 		attack = false;
 		transform.position = prevposition + new Vector3(0,-100,0);
 	}
--- 프로젝트 test/Test 2/Assets/CharacterStatus.cs
+++ 프로젝트 test/Test 2/Assets/CharacterStatus.cs
@@ -5,15 +5,23 @@ public class CharacterStatus : MonoBehaviour {
 	// status
 	public int HP = 100;
 	public int MaxHP = 100;
-
 	public int Power = 10;
+	bool Hit = false;
+	CharacterStatus characterStatus;
 
 	// name
 	public string characterName = "Enemy1";
+	//충돌 체크 
 	void OnTriggerEnter(Collider other)
 	{
-		HP -= 10;
-		print ("Hit");
+		characterStatus = GetComponent<CharacterStatus>();
+		if (!Hit) {
+			Hit = true;
+			//other 캐릭터의 파워만큼 감소
+			characterStatus.HP -= characterStatus.Power;
+			print ("Hit");
+			Hit = false;
+		}
 	}
 	
 }
