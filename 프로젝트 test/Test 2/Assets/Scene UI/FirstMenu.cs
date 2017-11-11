a3a9b89ad86df8980aea930a3ae0cae6741d68f0
Kang Hyun Woong <cdcd8894@naver.com>
2017-11-11 19:50:04 +0900

    2017 11 11 UI작업내용
    UI 기본
    빼빼로같은건 없당.

---
+++ 프로젝트 test/Test 2/Assets/Scene UI/FirstMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstMenu : MonoBehaviour {
+	public int UIWidth = 300;
+	public int UIHeight = 70;
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - (UIWidth / 2), Screen.height / 2 - 135 - (UIHeight / 2), UIWidth, UIHeight), "새로시작"))
+			Debug.Log ("New Game");
+		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 - 45 -(UIHeight/2), UIWidth, UIHeight), "이어하기"))
+			Debug.Log ("Load Game");
+		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 + 45 -(UIHeight/2), UIWidth, UIHeight), "멀티플레이"))
+			Debug.Log ("Multi Game");
+		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 + 135 -(UIHeight/2), UIWidth, UIHeight), "옵션"))
+			Debug.Log ("Option");
+	}
+}
