77fec53def67dab2cb3d0fa20bf46f701b96c107
Kang Hyun Woong <cdcd8894@naver.com>
2017-11-11 20:08:03 +0900

    2017 11 11
    UI와 신 연계

--- 프로젝트 test/Test 2/Assets/Scene UI/FirstMenu.cs
+++ 프로젝트 test/Test 2/Assets/Scene UI/FirstMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FirstMenu : MonoBehaviour {
 	public int UIWidth = 300;
@@ -8,9 +9,9 @@ public class FirstMenu : MonoBehaviour {
 	void OnGUI()
 	{
 		if (GUI.Button (new Rect (Screen.width / 2 - (UIWidth / 2), Screen.height / 2 - 135 - (UIHeight / 2), UIWidth, UIHeight), "새로시작"))
-			Debug.Log ("New Game");
+			SceneManager.LoadScene("map&player");
 		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 - 45 -(UIHeight/2), UIWidth, UIHeight), "이어하기"))
-			Debug.Log ("Load Game");
+			SceneManager.LoadScene("map&player");
 		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 + 45 -(UIHeight/2), UIWidth, UIHeight), "멀티플레이"))
 			Debug.Log ("Multi Game");
 		if(GUI.Button (new Rect  (Screen.width/2-(UIWidth/2), Screen.height/2 + 135 -(UIHeight/2), UIWidth, UIHeight), "옵션"))
